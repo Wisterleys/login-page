4ce63c2cf6b9a61536fc0084e26986eff5538894
Wisterleys Sousa <wister.sousa2@gmail.com>
2021-04-18 14:29:49 -0300

    Método para realizar busca com filtro de campos especificos no DB

--- Model/DbR.js
+++ Model/DbR.js
@@ -5,6 +5,7 @@ class DbR{
             this._db=db;//Só para verficar se a conexão com o DB foi bem sucedida ou não
             this._table=table;
             this.ok()//Confirmação da conexão bem sucedida ou não
+            this.impTest();
             //this.newTable()
     }
     newTable(){//CRIA NOVA TABELA
@@ -20,28 +21,33 @@ class DbR{
         })
     }
     // R
-    async selectAll(){
-        let res = await this.table.findAll()
-        return res;
-    }
-    // IMP
-    impAll(){
-        this.selectAll()
-        .then(res=>{
-            res.forEach(user=>{
-                    console.log(user.dataValues)
-            })
+    async selectAll(vetor=false){
+        let users=[]
+        let res = vetor?await this.table.findAll({attributes: vetor}):await this.table.findAll()
+        res.forEach(user=>{
+            users.push(user.dataValues)
         })
+        return users;
+    }
+    async select(id){
+        let res = await this.table.findByPk(id)
+        return res.dataValues;
     }
     //Método para confirmação de conexão
     async ok(){
         try {
             await this.db.authenticate();
             console.log('Banco de dados conectado com sucesso!.');
-           
+            
           } catch (error) {
             console.error('Erro na conexão ao DB:', error);
           }
+
+    }
+   async impTest(){
+    let res = await this.selectAll(["login","password"]);
+    console.log(res)
+        
     }
     //SETs and GETs
     get table(){
