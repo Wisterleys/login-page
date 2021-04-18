789efe5d60181fddb104ffb4131d0a096ca15a80
Wisterleys Sousa <wister.sousa2@gmail.com>
2021-04-18 12:29:40 -0300

    Alterei a estrutura da tabela trocando o compo nome para login e senha para password

--- tables/tables.js
+++ tables/tables.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 const db = require("../Model/conf/confDbR");
 
 const data=db.define("cadastros",{//Modelo de tabela a ser criada e consultada no bando de dados
-        nome:Sequelize.STRING,
-        senha:Sequelize.STRING
+        login:Sequelize.STRING,
+        password:Sequelize.STRING
 })
 module.exports=data;
