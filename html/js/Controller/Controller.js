5337baaa39c98a1cd0e822a8cab9b641d09d46a9
Wisterleys Sousa <wister.sousa2@gmail.com>
2021-04-18 20:48:58 -0300

    Pagina de login funcionando. Usando NODE e ORM Sequelize

--- html/js/Controller/Controller.js
+++ html/js/Controller/Controller.js
@@ -13,7 +13,9 @@ class Controler{
             !field.ok&&!this.btn?this.prints(field.name):0
             field.ok?$("button")[0].disabled=true:$("button")[0].disabled=false
         })
-        
+        if($("button")[0].disabled){
+            this.ajax($("#login").value)
+        }
     }
     checks(){
         let v=[]
@@ -22,6 +24,25 @@ class Controler{
             e.value?v.push({name:e.name,ok:true,index:i}):v.push({name:e.name,ok:false,index:i})
         });
         return v;
+    }
+    ajax(f){
+        let ajax = new XMLHttpRequest()
+        ajax.open("GET","http://localhost/search/"+f);
+        ajax.send();
+
+        ajax.onload=e=>{
+            let obj = JSON.parse(ajax.responseText)[0]?JSON.parse(ajax.responseText):false
+            if(obj){
+                $("#info").innerHTML="Logado!"
+            }
+            else{
+                $("#info").innerHTML="Login não existe";
+                $("button")[0].disabled=false
+                $(".check").forEach(e=>{
+                    e.style.background="red"
+                })
+            }
+        }
     }
     initial(){
         $("#button").on("click",e=>{
